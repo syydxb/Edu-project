964d1f0c5a1947c2ee6f1c4fa04ba3ce2ca4a8de
syydxb <1327396299@qq.com>
2024-01-14 20:38:30 +0800

    新增接口文档

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -1,5 +1,6 @@
 package com.xuecheng.base.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
@@ -13,8 +14,10 @@ import lombok.ToString;
 @ToString
 public class PageParams {
     //当前页码
+    @ApiModelProperty("当前页码")
     private Long pageNo = 1L;
     //每页显示记录数
+    @ApiModelProperty("每页记录数默认值")
     private Long pageSize = 10L;
 
     public PageParams() {
